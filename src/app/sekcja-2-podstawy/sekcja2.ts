c190347fc945aa6f8b301ad7f4fb8aca34fd8066
Maciej <maciek333programowanie@gmail.com>
2026-09-25 12:57:25 +0200

    add ng-content info

--- src/app/sekcja-2-podstawy/sekcja2.ts
+++ src/app/sekcja-2-podstawy/sekcja2.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './src/header/header';
 import { UserList } from './src/user-list/user-list';
 import { Sekcja2Form } from './src/sekcja2-form/sekcja2-form';
+import { Sekcja2Ngcontent } from './src/sekcja2-ngcontent/sekcja2-ngcontent';
 
 @Component({
   selector: 'app-sekcja-2',
-  imports: [Header, UserList, Sekcja2Form],
+  imports: [Header, UserList, Sekcja2Form, Sekcja2Ngcontent],
   template: `
     <div class="sekcja-2">
       <app-header />
@@ -13,6 +14,13 @@ import { Sekcja2Form } from './src/sekcja2-form/sekcja2-form';
       <app-user-list />
       <hr />
       <app-sekcja2-form />
+      <hr />
+      <app-sekcja2-ngcontent>
+        <p>Treść_1</p>
+      </app-sekcja2-ngcontent>
+      <app-sekcja2-ngcontent>
+        <p>Treść_2</p>
+      </app-sekcja2-ngcontent>
     </div>
   `,
   styles: `
---
+++ src/app/sekcja-2-podstawy/src/sekcja2-ngcontent/sekcja2-ngcontent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sekcja2-ngcontent',
+  imports: [],
+  templateUrl: './sekcja2-ngcontent.html',
+  styleUrl: './sekcja2-ngcontent.css',
+})
+export class Sekcja2Ngcontent {}
